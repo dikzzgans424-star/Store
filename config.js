499652cec033c2cf0e1bdd1d915d1995bb240f88
dikzzgans424-star <dikzzgans424@gmail.com>
2026-06-07 17:35:00 +0700

    Update PANEL_URL to new domain

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // 1. CUKUP GANTI INI JIKA PANEL KAMU GANTI DOMAIN
-    PANEL_URL: "https://erine.jkt48-private.com:4510", 
+    PANEL_URL: "https://erine.jkt48-private.com:3745", 
     
     // 2. Harus sama persis dengan SECRET_KEY di file webapi.js bot
     SECRET_KEY: "dimas_ganteng_123" 
